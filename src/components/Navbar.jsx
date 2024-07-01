3b5ad478a455d44f3bc89d94fffce9296e80d3ba
Rhey-Ranido <ranidochanchan@gmail.com>
2024-07-01 14:17:47 +0800

    this is my first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { useMediaQuery } from 'react-responsive';
+
+export default function Navbar() {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
+  return (
+    <nav>
+        {/* desktop */}
+        {!isMobile && (
+          <ul>
+              <li><a href="#">Works</a></li>
+              <li><a href="#">Blogs</a></li>
+              <li><a href="#">Contact</a></li>
+          </ul>
+        )}
+        {/* mobile */}
+        {isMobile && (
+           <div className='hamburgerMenu'>
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
+              <path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z"></path>
+              </svg>
+           </div>
+        )}
+    </nav>
+  )
+}
